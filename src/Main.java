239ed320ca98f66666f4df6c7e885e529f96cf35
rm87110 <pedrollss1911@gmail.com>
2023-05-19 09:47:37 -0300

    criação instanciação comportamento definição de objetos;

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         // aula introdutória de orientação à objetos
-        
+        //criar objetos
+        Pessoa adao = new Pessoa();
+        //declaação de objetos
+        Pessoa qualquer;
+        //instanciação do obtjeto
+        qualquer = new Pessoa();
+        Pessoa adulta = new Pessoa();
+        //definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        //definir comportamento do objeto
+        qualquer.falar();
+        adao.falar(" baixo");
+
+
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,12 @@
 public class Pessoa {
+    //atributos da classe
     String nome;
+    String sobrenome;
+    //métodos da classe
+    public void falar() {
+       System.out.println("falei");
+    }
+    public void falar(String volume) {
+        System.out.println("falei" + volume);
+    }
 }
